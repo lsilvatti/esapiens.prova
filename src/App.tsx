1036e7356a851aed493ee8c82058c6a6b7ccc489
Leonardo Silvatti <leosilvatti0@gmail.com>
2021-06-24 01:08:02 -0300

    added routes for search and users

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import User from 'pages/User';
-import Search from 'pages/Search';
+import Profile from 'pages/Profile';
+import NotFound from 'pages/NotFound';
+import Home from 'pages/Home';
+import Search from 'pages/Search'
 import { Fragment } from 'react';
 import bg from 'assets/img/bg.svg';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, useRouteMatch } from 'react-router-dom'
 
 function App() {
+
   return (
     <Fragment>
         <Router>
             <Switch>
-              <Route exact path="/"><Search /></Route>
-              <Route exact path="/user"><User /></Route>
+              <Route exact path="/"><Home /></Route>
+              <Route path={`/user/:userId`}><Profile /></Route>
+              <Route exact path="/search"><Search /></Route>
+              <Route><NotFound /></Route>
             </Switch>
         </Router>
     </Fragment>
