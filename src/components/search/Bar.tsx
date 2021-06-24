d144efc54f28c55df3c7638c9f4e6e6c391cf2b5
Leonardo Silvatti <leosilvatti0@gmail.com>
2021-06-24 01:08:49 -0300

    added button to searchbar

--- src/components/search/Bar.tsx
+++ src/components/search/Bar.tsx
@@ -1,11 +1,19 @@
 import { searchBarStyles } from './styles';
 import SearchField from './field';
+import SearchButton from './Button';
+import { useState } from 'react';
 
 export default function SearchBar() {
     const classes = searchBarStyles();
+    const [search, setSearch] = useState("");
+    const textChange = (text:string) => {
+        setSearch(text);
+    }
+    
     return (
         <div className={classes.root}>
-            <div className={classes.container}><SearchField /></div>
+            <div className={classes.container}> <SearchField fieldChange={textChange}/>
+            <SearchButton query={search} /></div>
         </div>
     )
 }
