62307b5cd7b5c520c8fcc127f77ea65a98edac27
Leonardo Silvatti <leosilvatti0@gmail.com>
2021-06-24 01:10:14 -0300

    not found page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div style={{height: "100%", width: "100%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+            <img src="https://http.cat/404" />
+            <a href="/">Voltar para home</a>
+        </div>
+    )
+}
