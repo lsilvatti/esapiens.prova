8b53936d0b3d541efe99038326d062a478df3103
Leonardo Silvatti <leosilvatti0@gmail.com>
2021-06-24 01:10:52 -0300

    added some styles for  home

--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -2,12 +2,9 @@ import {createUseStyles} from 'react-jss';
 
 
 export const userPageStyles = createUseStyles({
-    root:{
-        
-    }
 })
 
-export const searchStyles = createUseStyles({
+export const homeStyles = createUseStyles({
     root:{
         width:"100%",
         height: "100%",
@@ -16,9 +13,16 @@ export const searchStyles = createUseStyles({
         alignItems: "center"
     },
     container:{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         '@media (min-width: 425px)':{
             width: "30vw",
         },
-        width:"100%"
-    }
+        "& img":{
+            maxWidth: 250,
+            marginBottom: 100
+        }
+    },
+
 })
