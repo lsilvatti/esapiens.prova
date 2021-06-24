0d7d8e8419c6650f22032a1b54bd6bda9a518eda
Leonardo Silvatti <leosilvatti0@gmail.com>
2021-06-24 01:07:27 -0300

    loading component

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { PacmanLoader } from "react-spinners";
+import { loadingStyles } from './styles';
+export default function Loading(){
+
+    const classes = loadingStyles();
+    const [color, setColor] = useState("#1F7A8C");
+
+    return (
+        <div className={classes.root}>
+            <PacmanLoader color={color} size={30} />
+        </div>
+        
+    )
+}
