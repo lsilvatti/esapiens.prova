c0058d448c6c03247e4e20301c3f7eb786da852f
Leonardo Silvatti <leosilvatti0@gmail.com>
2021-06-25 00:40:14 -0300

    enter searches field

--- src/components/search/field.tsx
+++ src/components/search/field.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { searchFieldStyles } from './styles';
 
 interface SearchProps {
     fieldChange: (text: string) => void
 }
 
-export default function SearchField(props: SearchProps){
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+  }
 
+export default function SearchField(props: SearchProps){
+    
+    const query = useQuery();
     const { fieldChange } = props;
 
     const [value, setValue] = useState("");
@@ -16,11 +22,22 @@ export default function SearchField(props: SearchProps){
         fieldChange(event.target.value);
     }
 
+    function keyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key == "Enter") {
+            if(value !== ""){
+                window.location.assign(`/search?user=${value}&page=1`);
+            }
+        }
+      }
+
+    useEffect(() =>{
+        setValue(query.get("user") ?? "");
+    },[])
     const classes = searchFieldStyles();
 
     return (
         <div className={classes.root}>
-            <input id="field" placeholder="Pesquisar usuário" value={value} onChange={handleChange}  type="text" />
+            <input id="field" placeholder="Pesquisar usuário" onKeyPress={keyPress} value={value} onChange={handleChange}  type="text" />
         </div>
         )
 }
