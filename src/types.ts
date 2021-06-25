055c2442587ab961e1d420902f6fc6757bc3e20d
Leonardo Silvatti <leosilvatti0@gmail.com>
2021-06-25 00:38:27 -0300

    added owner

--- src/types.ts
+++ src/types.ts
@@ -41,7 +41,7 @@ export interface User {
 export interface UserSearchResponse {
     total_count: number;
     incomplete_results: boolean;
-    items: User[];
+    items: UserSearch[];
 }
 
 export interface Repository {
@@ -139,4 +139,26 @@ export interface Owner {
     received_events_url: string;
     type: string;
     site_admin: boolean;
+}
+
+export interface UserSearch {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    gists_url: string;
+    starred_url: string;
+    subscriptions_url: string;
+    organizations_url: string;
+    repos_url: string;
+    events_url: string;
+    received_events_url: string;
+    type: string;
+    site_admin: boolean;
+    score: number;
 }
