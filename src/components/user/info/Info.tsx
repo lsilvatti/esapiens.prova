fe94877ec63033950646ea12fbce8b6fd7454628
Leonardo Silvatti <leosilvatti0@gmail.com>
2021-06-25 00:40:33 -0300

    added link to info

--- src/components/user/info/Info.tsx
+++ src/components/user/info/Info.tsx
@@ -12,19 +12,21 @@ export default function UserInfo(props: UserInfoProps){
     return (
         <div className={classes.root}>
             <div className={classes.container}>
-                <ProfilePic url={user.avatar_url} login={user.login} />
+                <div className={classes.imgContainer}><ProfilePic url={user.avatar_url} login={user.login} /></div>
                 <div className={classes.info}>
                     <Fragment>
-                    <h1>{user.name}</h1>
+                    <h1>{user.name ?? ""}</h1>
                     <span>{user.login}</span>
                     <h4>{user.location}</h4>
                     <h4>{user.email}</h4>
                     <p>{user.bio}</p>
+                    
                     </Fragment>
                    
                         <p>
                             <span>{user.followers} seguidores</span> · <span>{user.following} seguindo</span>
                         </p>
+                        <a className={classes.link} href={user.html_url} target="_blank">Visitar perfil no Github</a>
                 </div>
             </div>
             
